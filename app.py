d8f8073ac8be9fa0f60eb382dfa6b2ae6c9a4988
jay kumar gupta <jaynrk2002@gmail.com>
2024-06-15 11:12:38 +0530

    requirements.txt added

--- app.py
+++ app.py
@@ -113,10 +113,3 @@ def hello_name():
 def handle_error(e):
     print(f"An error occurred: {e}")
     return jsonify(error=str(e)), 500
-
-if __name__ == '__main__':
-    print("Initializing Flask app...")
-    try:
-        app.run(debug=False, host='0.0.0.0')
-    except Exception as e:
-        print(f"An error occurred during app.run(): {e}")
