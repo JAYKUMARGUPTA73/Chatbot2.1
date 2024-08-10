3748464750dbeb7c5a02cb3cd17b98dd0a580c0c
jay kumar gupta <jaynrk2002@gmail.com>
2024-08-10 09:46:03 +0530

    ui updated

--- app.py
+++ app.py
@@ -3,6 +3,9 @@
 Created on Fri Jun 14 10:46:51 2024
 @author: Jay kumar gupta
 """
+
+from flask_cors import CORS
+
 import sys
 sys.excepthook = lambda type, value, traceback: print(f"Uncaught exception: {value}")
 
@@ -16,6 +19,7 @@ import numpy as np
 from tensorflow.keras.models import load_model
 import json
 import random
+from trainchatboat import train_chatbot 
 
 print("Loading chatbot components...")
 try:
@@ -62,9 +66,9 @@ def bow(sentence, words, show_details=True):
     return(np.array(bag))
 
 def predict_class(sentence, model):
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.4
+    ERROR_THRESHOLD = 0.1
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
@@ -89,27 +93,82 @@ def chatbot_response(msg):
     res = getResponse(ints, intents)
     return res
 
-''' Flask code '''
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET', 'POST'])
-def hello():
-    return jsonify({"key" : "home page value"})
+CORS(app)
+
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return jsonify({"key": "home page value"})
 
 def decrypt(msg):
     return msg.replace("+", " ")
 
+
+@app.route('/home/update', methods=['POST'])
+def recompile_bot():
+    print("hey")
+
+    data = request.get_json()
+    
+    pro_city = data.get('pro_city')
+    pro_locality = data.get('pro_locality')
+    pro_amt = data.get('pro_amt')
+    pro_area_size = data.get('pro_area_size')
+    pro_desc = data.get('pro_desc')
+    link=data.get('link')
+
+    # Process the data as needed
+    patterns = [
+        f"Tell me about a property in {pro_city}",
+        f"What are the details of the property in {pro_locality}?",
+        f"How much does the property in {pro_city} cost?",
+        f"Describe the property located in {pro_locality}",
+        f"What is the area size of the property in {pro_city}?",
+        f"Give me details about a {pro_area_size} sqft property in {pro_city}",
+        f"How many bedrooms in the property located at {pro_locality}?",
+        f"Is there a property in {pro_city} with a budget of {pro_amt}?",
+        f"Details of a property in {pro_locality} with {pro_amt} price",
+        f"Can you provide the description of a property in {pro_city}?"
+    ]
+    intent_data = {
+        "tag": "property",
+        "patterns": patterns,
+        "responses": [
+            "Here are the details of the property:",
+            f"City: {pro_city}",
+            f"Locality: {pro_locality}",
+            f"Price: {pro_amt}",
+            f"Area Size: {pro_area_size} sqft",
+            f"Description: {pro_desc}"
+        ],
+        "context": [f"This is link from which you can search the property :{link}"]
+    }
+    with open('intent.json', 'r+', encoding='utf-8') as file:
+        data = json.load(file)
+        data['intents'].append(intent_data)
+        file.seek(0)  # Move the cursor to the beginning of the file
+        json.dump(data, file, indent=4)
+        file.truncate() 
+    train_chatbot()
+    return jsonify(data), 200
+
 @app.route('/home', methods=['GET'])
-def hello_name():
+def home_page():
     name = request.args.get('name', '')
     dec_msg = decrypt(name)
     response = chatbot_response(dec_msg)
-    json_obj = jsonify({"top" : {"res" : response}})
+    json_obj = jsonify({"top": {"res": response}})
     return json_obj
 
 @app.errorhandler(Exception)
 def handle_error(e):
     print(f"An error occurred: {e}")
     return jsonify(error=str(e)), 500
+
+if __name__ == '__main__':
+    app.run(debug=False)
--- trainchatboat.py
+++ trainchatboat.py
@@ -1,112 +1,102 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 13 13:29:21 2024
-@author: Jay kumar gupta
-"""
-#import statements
 import nltk
-nltk.download('punkt')          
-nltk.download('wordnet')       
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-#core model libraries
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
 import numpy as np
 import random
+import tensorflow as tf
+from tqdm import tqdm
+import os
 
-#Initailisation list
-words = []
-classes = []
-documents = []
-ignore_words = ['?','!','@','$']
+nltk.download('punkt', quiet=True)
+nltk.download('wordnet', quiet=True)
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
-#use json
-data_file = open('intent.json', encoding="utf8").read()
-intents = json.loads(data_file)
+def train_chatbot():
+    words = []
+    classes = []
+    documents = []
+    ignore_words = ['?', '!', '@', '$']
 
-#Populating the list
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
-        w = nltk.word_tokenize(pattern)
-        words.extend(w)
-        documents.append((w, intent['tag']))
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
+    data_file = open('intent.json', encoding="utf8").read()
+    intents = json.loads(data_file)
 
-words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
-words = sorted(list(set(words)))
-classes = sorted(list(set(classes)))
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            w = nltk.word_tokenize(pattern)
+            words.extend(w)
+            documents.append((w, intent['tag']))
+            if intent['tag'] not in classes:
+                classes.append(intent['tag'])
+                
+    if os.path.exists('words.pkl') and os.path.exists('classes.pkl'):
+        words = pickle.load(open('words.pkl', 'rb'))
+        classes = pickle.load(open('classes.pkl', 'rb'))
+    else:
+        words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+        words = sorted(list(set(words)))
+        classes = sorted(list(set(classes)))
+        pickle.dump(words, open('words.pkl', 'wb'))
+        pickle.dump(classes, open('classes.pkl', 'wb'))
 
-pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(classes, open('classes.pkl', 'wb'))
+    training = []
+    output_empty = [0] * len(classes)
+    for doc in documents:
+        bag = []
+        pattern_words = doc[0]
+        pattern_words = [lemmatizer.lemmatize(w.lower()) for w in pattern_words]
+        
+        for w in words:
+            bag.append(1) if w in pattern_words else bag.append(0)
+        
+        output_row = list(output_empty)
+        output_row[classes.index(doc[1])] = 1
+        
+        training.append([bag, output_row])
 
-#training the bot
-training = []
-output_empty = [0] * len(classes)
-for doc in documents:
-    bag = []
-    pattern_words = doc[0]
-    pattern_words = [lemmatizer.lemmatize(w.lower()) for w in pattern_words]
-    
-    for w in words:
-        bag.append(1) if w in pattern_words else bag.append(0)
-    
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
-    
-    training.append([bag, output_row])
+    random.shuffle(training)
+    training = np.array(training, dtype=object)
 
-random.shuffle(training)
-training = np.array(training, dtype=object)
+    train_x = np.array([item[0] for item in training])
+    train_y = np.array([item[1] for item in training])
 
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(256, input_shape=(len(train_x[0]),), activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(len(train_y[0]), activation='softmax')
+    ])
 
+    model.compile(loss='categorical_crossentropy', 
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), 
+                  metrics=['accuracy'])
 
+    print("Training model...")
+    epochs = 500
+    batch_size = 32
 
-# Convert training data to TensorFlow tensors
-train_x = tf.convert_to_tensor(train_x, dtype=tf.float32)
-train_y = tf.convert_to_tensor(train_y, dtype=tf.float32)
+    history = model.fit(
+        train_x, train_y,
+        epochs=epochs,
+        batch_size=batch_size,
+        verbose=0,
+        validation_split=0.1
+    )
 
-# Define the model using TensorFlow's low-level API
-def create_model():
-    inputs = tf.keras.Input(shape=(len(train_x[0]),))
-    x = tf.keras.layers.Dense(128, activation='relu')(inputs)
-    x = tf.keras.layers.Dropout(0.5)(x)
-    x = tf.keras.layers.Dense(64, activation='relu')(x)
-    x = tf.keras.layers.Dropout(0.5)(x)
-    outputs = tf.keras.layers.Dense(len(train_y[0]), activation='softmax')(x)
-    return tf.keras.Model(inputs=inputs, outputs=outputs)
+    for epoch in tqdm(range(epochs), desc="Training Progress"):
+        loss, accuracy = model.evaluate(train_x, train_y, verbose=0)
+        if (epoch + 1) % 50 == 0:
+            print(f"\nEpoch {epoch + 1}/{epochs}")
+            print(f"Loss: {loss:.4f}")
+            print(f"Accuracy: {accuracy:.4f}")
 
-model = create_model()
+    model.save('chatbot_model.h5')
+    print("Model created successfully")
 
-# Prepare the dataset
-dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y)).shuffle(1000).batch(5)
+    return model, words, classes
 
-# Define the optimizer and loss function
-optimizer = tf.keras.optimizers.SGD(learning_rate=0.01, momentum=0.9)
-loss_fn = tf.keras.losses.CategoricalCrossentropy()
-
-# Training loop
-@tf.function
-def train_step(x, y):
-    with tf.GradientTape() as tape:
-        logits = model(x, training=True)
-        loss_value = loss_fn(y, logits)
-    grads = tape.gradient(loss_value, model.trainable_weights)
-    optimizer.apply_gradients(zip(grads, model.trainable_weights))
-    return loss_value
-
-epochs = 200
-for epoch in range(epochs):
-    print(f"\nStart of epoch {epoch+1}")
-    for step, (x_batch_train, y_batch_train) in enumerate(dataset):
-        loss_value = train_step(x_batch_train, y_batch_train)
-        if step % 200 == 0:
-            print(f"Training loss (for one batch) at step {step}: {float(loss_value):.4f}")
-
-# Save the model
-model.save('chatbot_trained.h5')
-print("model_created_successfully")
+# You can call this function from app.py when recompile function is called
+# model, words, classes = train_chatbot()
